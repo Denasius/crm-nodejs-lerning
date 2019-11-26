9a1a588c8f3900457069b3bedde24105964a2634
Denasius <den_kostyuk1989@mail.ru>
2019-11-26 23:30:29 +0300

    created registration method

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs')
 const User = require('../models/User')
 
 module.exports.login = function (request, response) {
@@ -9,13 +10,26 @@ module.exports.login = function (request, response) {
 	})
 }
 
-module.exports.register = function (request, response) {
-	const user = new User({
-		email: request.body.email,
-		password: request.body.password
-	})
+module.exports.register = async function (request, response) {
+	const userCandidate = await User.findOne({email:request.body.email})
 
-	user.save()
-		.then(() => console.log('User is created'))
-		.catch(error => console.log(error))
+	if ( userCandidate ){
+		// status 409 is CONFLICT
+		response.status(409).json({
+			message: 'Такой Email уже занят. Попробуйте другой.'
+		})
+	}else{
+		const salt = bcrypt.genSaltSync(10)
+		const newUser = new User({
+			email: request.body.email,
+			password: bcrypt.hashSync(request.body.password, salt)
+		})
+
+		try{
+			await newUser.save()
+
+			// status 201 is CREATED
+			response.status(201).json(newUser)
+		}catch(e){}
+	}
 }
