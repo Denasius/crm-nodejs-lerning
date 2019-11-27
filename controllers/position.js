726a754d5ff1c9627821725555ec54bb6e11d169
Denasius <den_kostyuk1989@mail.ru>
2019-11-27 23:16:04 +0300

    method getByCategoryId in position.js

--- controllers/position.js
+++ controllers/position.js
@@ -1,7 +1,32 @@
-module.exports.getByCategoryId = function (request, response) {}
+const position = require('../models/Position')
+const errorHandler = require('../utils/errorHandler')
 
-module.exports.create = function (request, response) {}
+module.exports.getByCategoryId = async function (request, response) {
+	try{
+		const positions = await position.find({
+			category: request.params.categoryId,
+			user: request.user.id
+		})
+		response.status(200).json(positions)
+	}catch(error){
+		errorHandler(response, error)
+	}
+}
 
-module.exports.remove = function (request, response) {}
+module.exports.create = function (request, response) {
+	try{}catch(error){
+		errorHandler(response, error)
+	}
+}
 
-module.exports.update = function (request, response) {}
+module.exports.remove = function (request, response) {
+	try{}catch(error){
+		errorHandler(response, error)
+	}
+}
+
+module.exports.update = function (request, response) {
+	try{}catch(error){
+		errorHandler(response, error)
+	}
+}
