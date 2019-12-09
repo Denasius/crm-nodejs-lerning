a0dc95ba253a9669324cb9ed5b791bba415a9460
Denasius <den_kostyuk1989@mail.ru>
2019-12-09 22:09:43 +0300

    required login and register page and required layouts for auth

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 import { LoginPageComponent } from './login-page/login-page.component';
-import { AppRoutingModule } from './app-routing.module' 
+import { AppRoutingModule } from './app-routing.module';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+import { RegisterPageComponent } from './register-page/register-page.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    AuthLayoutComponent,
+    SiteLayoutComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
