d12882e7f7582f26f94fb47e888150f7485b0cf4
Denasius <den_kostyuk1989@mail.ru>
2019-12-13 23:48:36 +0300

    created login method in auth service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import {BrowserModule} from '@angular/platform-browser'
-import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import {AppComponent} from './app.component'
+import {AppComponent} from './app.component';
 import {LoginPageComponent} from './login-page/login-page.component';
 import {AppRoutingModule} from './app-routing.module';
 import {AuthLayoutComponent} from './shared/layouts/auth-layout/auth-layout.component';
 import {SiteLayoutComponent} from './shared/layouts/site-layout/site-layout.component';
 import {RegisterPageComponent} from './register-page/register-page.component'
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     AppRoutingModule,
     ReactiveFormsModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/shared/services/auth.services.ts
@@ -0,0 +1,43 @@
+import {Injectable} from '@angular/core';
+import {User} from './interface';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServices {
+  private token = null;
+  constructor(private http: HttpClient) {
+  }
+
+  login(user: User): Observable<{ token: string }> {
+
+    return this.http.post<{ token: string }>('/api/auth/login')
+      .pipe(tap(({token})=>{
+        localStorage.setItem('auth-token', token);
+        this.setToken(token);
+      }));
+  }
+
+  setToken(token: string){
+    this.token = token;
+  }
+
+  getToken(): string {
+    return this.token;
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.token;
+  }
+
+  logout(){
+    this.setToken(null);
+    localStorage.clear();
+  }
+
+  register() {
+  }
+}
