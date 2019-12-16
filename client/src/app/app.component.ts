f3b8e84f356f9423b9b314c5d64187ea5231e9cb
Denasius <den_kostyuk1989@mail.ru>
2019-12-16 21:31:41 +0300

    genereted interceptor

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthServices} from "./shared/services/auth.services";
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>'
 })
-export class AppComponent {}
+export class AppComponent implements OnInit{
+  constructor(private auth: AuthServices) {
+  }
+
+  ngOnInit(): void {
+    const potencialToken = localStorage.getItem('auth-token')
+
+
+    if ( potencialToken !== null ) {
+      this.auth.setToken(potencialToken)
+    }
+  }
+}
